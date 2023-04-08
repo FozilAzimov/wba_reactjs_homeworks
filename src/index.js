d111b7ec784618ed9d1f48d02a8acf75217e0722
FozilAzimov <foziljon.azimov@yandex.ru>
2023-04-08 21:38:38 +0500

    homeworks

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Section from './Homework_1/index';
+import './Homework_1/style.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Section />
+  </React.StrictMode>
+);
