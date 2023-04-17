b0464b7197d488bcc59569f716f2c87d1fe9d823
FozilAzimov <foziljon.azimov@yandex.ru>
2023-04-17 12:33:27 +0500

    homeworks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Section from './Homework_1/index';
-import './Homework_1/style.css';
 
+// 1-homework start
+// import Section from './Homework_1/index';
+// import './Homework_1/style.css';
+// 1-homework end
+
+// 2-homework start
+// import State from './Homework_2/index';
+// import './Homework_2/style.css';
+// 2-homework end
+
+// 3-homework start
+import Root from './Homework_3/root';
+import './index.css';
+// 3-homework end
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Section />
+    {/* <Section /> */ }
+
+    {/* <State /> */ }
+
+    {/* <Styled /> */ }
+
+    <Root />
   </React.StrictMode>
 );
