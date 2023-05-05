124f008467f7383d5de1fea91c54df53a6df7771
FozilAzimov <foziljon.azimov@yandex.ru>
2023-05-05 13:49:36 +0500

    homework_7

--- src/Homework_7/components/Navbar/index.jsx
+++ src/Homework_7/components/Navbar/index.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   Container,
   Item
 } from './style';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom';
 
 export default function Navbar() {
+
+  const his = useHistory();
+
   const [pathname, Pathname] = useState('home');
 
   const setName = ({ target: { innerText } }) => {
     Pathname(innerText.toLowerCase());
   }
+
+  const newUrl = new URL(window.location.href);
+  newUrl.searchParams.set("", pathname)
+
+  console.log(newUrl);
 
 
   return (
